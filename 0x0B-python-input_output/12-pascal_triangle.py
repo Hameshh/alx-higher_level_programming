67168db8dc552b59beab7b9bfb351ae12b0f10a2
Hameshh <jossy21w@yahoo.com>
2022-09-27 12:20:24 -0700

    Fixed it

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,23 +1,20 @@
 #!/usr/bin/python3
-"""
-pascal_triangle module
-"""
+"""definig function"""
 
 
 def pascal_triangle(n):
+    """Represent Pascal's Triangle of size n.
+    Returns a list of lists of integers representing the triangle.
     """
-    returns a list of lists of integers representing the Pascal’s triangle
-    """
-    tri = []
-    tmp = [1]
-    if n == 0:
-        return tri
+    if n <= 0:
+        return []
 
-    for i in range(n):
+    triangles = [[1]]
+    while len(triangles) != n:
+        tri = triangles[-1]
         tmp = [1]
-        for j in range(i):
-            tmp.append(i)
-        tmp[-1] = 1
-        tri.append(tmp)
-
-    return tri
+        for i in range(len(tri) - 1):
+            tmp.append(tri[i] + tri[i + 1])
+        tmp.append(1)
+        triangles.append(tmp)
+    return triangles
--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,19 +1,15 @@
 #!/usr/bin/python3
-"""
-load_add_save module
-"""
+"""module to load and save"""
 import sys
-import os
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file =\
+        __import__('6-load_from_json_file').load_from_json_file
 
-if os.path.exists('add_item.json'):
-    objs = load_from_json_file('add_item.json')
-else:
-    objs = []
-
-for n in range(1, len(sys.argv)):
-    objs.append(sys.argv[n])
-
-save_to_json_file(objs, 'add_item.json')
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
