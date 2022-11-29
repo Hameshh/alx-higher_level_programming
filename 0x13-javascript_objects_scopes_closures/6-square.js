3213757f3602e7fbbb5f504d8d097f5874652afa
josephine <44226281+Hameshh@users.noreply.github.com>
2022-11-29 23:13:31 +0100

    Master

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,13 +1,10 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle');
-
-module.exports = class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-    this.size = size;
-  }
-
-  charPrint (ch) {
-    super.print(ch);
+module.exports = class Square extends require('./5-square.js') {
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) console.log(c.repeat(this.width));
+    }
   }
 };
